2d7d71f771e5fcb56258a7d4aa9851da25a18ba3
RhenarSBR <contact@studioblackraven.fr>
2016-04-16 15:57:24 +0200

    New Lig

---
+++ lua/vgui/cl_close.lua
@@ -0,0 +1,19 @@
+-----------------------
+---- New Life Menu ----
+-----------------------
+--- Auteur : Rhenar ---
+--- Version : O.1   ---
+-----------------------
+
+local close = vgui.Create("DButton", general)
+close:SetText("PLAY")
+close:SetTextColor(Color(255,255,255,255))
+close:SetPos(ScrH() / 4, ScrW() * 4)
+close:SetSize(ScrH() / 500,ScrW() / 400)
+close.Paint = function( self, w, h)
+	draw.RoundedBox(0,0,0,w,h,Color(231,76,60,255))
+end
+close.DoClick = function()
+	hook.Call("NameChange")
+	general:SetVisible(false)
+end
--- lua/vgui/cl_name.lua
+++ lua/vgui/cl_name.lua
@@ -1,10 +1,31 @@
-----------------------
----- Create Perso ----
-----------------------
--- Auteur : Rhenar ---
--- Version : O.1   ---
-----------------------
+-----------------------
+---- New Life Menu ----
+-----------------------
+--- Auteur : Rhenar ---
+--- Version : O.1   ---
+-----------------------
+include("lua/vgui/cl_general.lua")
 
-local containername = vgui.Create("DPanel")
-containername:SetSize(ScrW() / 2,ScrH() / 2)
-containername:SetPos(ScrH() / 2, ScrW() / 2)
+local containername = vgui.Create("DPanel" general)
+containername:SetSize(ScrH(), ScrW())
+containername:Center()
+containername:MakePopup()
+containername.Paint = function(self,w,h)
+	draw.RoundedBox(0,0,0,w,h,Color(0,0,0,255))
+end
+
+local champtextnom = vgui.Create("DTextEntry", containername)
+champtextnom:Center()
+champtextnom:SetSize(ScrH() / 500,ScrW() / 400)
+champtextnom:SetText("Nom")
+champtextnom.OnTextChanged() = function(self)
+	nomc = self:GetValue()
+end
+
+local champtextprenom = vgui.Create("DTextEntry", containername)
+champtextprenom:SetPos(ScrH() / 6, ScrW() / 2)
+champtextprenom:SetSize(ScrH() / 500,ScrW() / 400)
+champtextprenom:SetText("Prenom")
+champtextprenom.OnTextChanged() = function(self)
+	prenomp = self.GetValue()
+end
